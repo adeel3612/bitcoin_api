d320afdc8276e0e6db1370e33f7252b0fc86c0e8
Adeel <adeel@esper.solutions>
2021-02-26 05:58:35 +0100

    set wrapper controller to access the controller and add the routes in api.php for routing

---
+++ bitcoinapi_backend/app/Http/Controllers/BitcoinController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BitcoinController extends Controller
+{
+    public function getBitCoinData(Request $request)
+    {
+
+        return $request;
+    }
+}
--- bitcoinapi_frontend/js/custom.js
+++ bitcoinapi_frontend/js/custom.js
@@ -1,4 +1,6 @@
 // initiate the js on the page load.
+var api_url = '/bitcoinapi_app/bitcoinapi_backend/public/api/getbitcoindata'
+
 $( document ).ready(function() {
     $('.datepicker_to').datepicker({
         format: 'mm-dd-yyyy',
@@ -11,4 +13,31 @@ $( document ).ready(function() {
     });
     $('.datepicker_to').datepicker('setDate', 'now');
     $('.datepicker_from').datepicker('setDate', '-10d');
+
+
+    //get data from the api on the page load
+    get_data_from_api();
+
+    $( ".render_button" ).on( "click", function() {
+        get_data_from_api();
+    });
 });
+
+function get_data_from_api(){
+    var date_to = $('.datepicker_to').val();
+    var date_from = $('.datepicker_from').val();
+
+    var data = {
+        'date_to' : date_to,
+        'date_from' : date_from,
+    };
+    $.ajax({
+        method: 'POST',
+        url: api_url,
+        data: data,
+        dataType: 'json',
+        success: function(result) {
+            console.log(result);
+        },
+    });
+}
